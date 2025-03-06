5a241874cbf28a57aa9ba137da150f280ea712d5
Imranf620 <imranf620@gmail.com>
2025-03-06 20:20:41 +0500

    fixed navbar responsiveness

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ const Main = () => {
   return (
     <>
     <div className=" flex md:flex-row flex-col pl-2 pr-4">
-      <div className="flex-3">
+      <div className="flex-3 mt-6 md:mt-0 ">
         <LeftMain />
       </div>
       <div className="flex-1 flex justify-center items-center w-full">
